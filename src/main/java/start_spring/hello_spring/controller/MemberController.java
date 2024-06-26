7672eb82cad699734a7a3d00031e7b7b73d5c531
Jjunjiii’ <drum1701@gmail.com>
2024-06-26 22:20:00 +0900

    members web

--- src/main/java/start_spring/hello_spring/controller/MemberController.java
+++ src/main/java/start_spring/hello_spring/controller/MemberController.java
@@ -3,8 +3,13 @@ package start_spring.hello_spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import start_spring.hello_spring.domain.Member;
 import start_spring.hello_spring.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -14,5 +19,27 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createFrom() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        System.out.println("Success Create : " + form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/membersList";
     }
 }

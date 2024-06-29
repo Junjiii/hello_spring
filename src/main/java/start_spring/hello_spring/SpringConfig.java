26752aba6bdb8973132595e3ef0e2d4706c2beb9
Jjunjiii’ <drum1701@gmail.com>
2024-06-29 21:59:16 +0900

    jdbc

--- src/main/java/start_spring/hello_spring/SpringConfig.java
+++ src/main/java/start_spring/hello_spring/SpringConfig.java
@@ -2,12 +2,22 @@ package start_spring.hello_spring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import start_spring.hello_spring.repository.JdbcMemberRepository;
 import start_spring.hello_spring.repository.MemberRepository;
 import start_spring.hello_spring.repository.MemoryMemberRepository;
 import start_spring.hello_spring.service.MemberService;
 
+import javax.sql.DataSource;
+
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,6 +25,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
